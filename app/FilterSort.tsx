1f4999da33c731d6f5813d0be9897e512d9092aa
titusefferian <tituseff@gmail.com>
2023-12-14 15:42:47 +0900

    finishing

--- app/FilterSort.tsx
+++ app/FilterSort.tsx
@@ -7,34 +7,36 @@ import { useCallback } from 'react';
 const FilterSort = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const { push, refresh } = useRouter();
+  const { push } = useRouter();
   const handleOnClickKoruptor = useCallback(() => {
     const newParams = new URLSearchParams(searchParams);
     if (newParams.get('corrupted') === 'true') {
       newParams.delete('corrupted');
       push(`${pathname}?${newParams}`);
-      refresh();
       return;
     }
     newParams.set('corrupted', 'true');
     push(`${pathname}?${newParams}`);
-    refresh();
-  }, [pathname, push, refresh, searchParams]);
+  }, [pathname, push, searchParams]);
+
+  const handleClickSemua = useCallback(() => {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.delete('corrupted');
+    push(`${pathname}?${newParams}`);
+  }, [pathname, push, searchParams]);
 
   const isCorruptedChecked = Boolean(searchParams.get('corrupted')) ?? false;
 
   return (
     <Stack>
       <Group>
+      <Chip checked={!isCorruptedChecked} onChange={handleClickSemua}>
+          Semua
+      </Chip>
         <Chip checked={isCorruptedChecked} onChange={handleOnClickKoruptor}>
           Mantan Koruptor
         </Chip>
       </Group>
-      {/* <Group>
-            <Chip>
-                PDIP
-            </Chip>
-        </Group> */}
     </Stack>
   );
 };
